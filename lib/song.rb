a8fe9833644f67e7167c2a426d72a835c603a61d
Allen F Partlow <allenfp@gmail.com>
2019-03-15 03:07:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,13 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@count = []
+  @@count = 0
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end
 
 end
